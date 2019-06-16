f2730e621e6fd29921aa70d06c394178a581198f
asifahmed90 <asifahmed90@users.noreply.github.com>
2019-06-15 23:09:04 -0500

    Initial Commit for maven project

---
+++ src/main/java/co/getmehired/getmehired/GetmehiredApplication.java
@@ -0,0 +1,13 @@
+package co.getmehired.getmehired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmehiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmehiredApplication.class, args);
+	}
+
+}
