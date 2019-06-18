183fbf15b99782186f8254b5affd330b75864eec
asifahmed90 <asifahmed90@users.noreply.github.com>
2019-06-18 02:28:48 -0500

    New commit version 3

---
+++ src/main/java/co/getmehired/getmehired/GetMeHiredApplication.java
@@ -0,0 +1,13 @@
+package co.getmehired.getmehired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetMeHiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetMeHiredApplication.class, args);
+	}
+
+}
