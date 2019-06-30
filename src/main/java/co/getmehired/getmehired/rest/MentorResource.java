1ea956759ff768fb572373da9de0f735e4669033
Tasmia <orni_254@yahoo.com>
2019-06-29 20:43:59 -0400

    Mentor is fully implemented

--- src/main/java/co/getmehired/getmehired/rest/MentorResource.java
+++ src/main/java/co/getmehired/getmehired/rest/MentorResource.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import co.getmehired.getmehired.model.Mentor;
 import co.getmehired.getmehired.model.dto.MentorDTO;
+import co.getmehired.getmehired.repository.MentorRepository;
 import co.getmehired.getmehired.service.MentorService;
 
 @RestController
@@ -24,7 +28,7 @@ public class MentorResource {
 	public Mentor saveMentor(@RequestBody Mentor mentor) {
 	
 		 /// saving the input of mentor in the repository
-		mentor=mentorService.save(mentor);
+		mentor=mentorService.saveMentor(mentor);
 		return mentor;
 	}
 	
@@ -46,6 +50,43 @@ public class MentorResource {
 	    
     }
 	
+	@GetMapping("/api/mentors/{id}")
+	public MentorDTO getMentorById (@PathVariable String id) {
+		Mentor m=mentorService.getMentorsById(id).orElseGet(null);
+	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+                  m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+                  m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+                  m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+	    return m_dto;
+		
+		
+	}
+	@PutMapping("/api/mentors/{id}")
+	public MentorDTO updateMentor(@PathVariable String id,@RequestBody Mentor m) {
+		
+		Mentor old_mentor=mentorService.getMentorsById(id).orElseGet(null);
+	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+                 m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+                 m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+                 m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+	    mentorService.saveMentor(m);
+	    mentorService.removeMentorById(id);
+	    return m_dto;	
+	}
+	
+	@DeleteMapping("/api/mentors/{id}")
+	public MentorDTO deleteMentor(@PathVariable String id){
+      
+		Mentor m=mentorService.getMentorsById(id).orElseGet(null);
+	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+                m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+                m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+                m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+	    mentorService.removeMentorById(id);
+	    return m_dto;
+	    
+	    
+	}
 	
 
 }
--- src/main/java/co/getmehired/getmehired/service/MentorService.java
+++ src/main/java/co/getmehired/getmehired/service/MentorService.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,7 @@ public class MentorService {
 	@Autowired
 	private MentorRepository mentorrepository;
 	
-	public Mentor save(Mentor mentor) {
+	public Mentor saveMentor(Mentor mentor) {
       
 		return mentorrepository.save(mentor);
 	}
@@ -26,7 +28,13 @@ public class MentorService {
 		return mentorrepository.findAll();
 	}
 	
+	public Optional<Mentor> getMentorsById(String id) {
+		return mentorrepository.findById(id);
+	}
 	
-	
-
+    public Optional< Mentor> removeMentorById(String id) {
+    	Optional<Mentor> m=mentorrepository.findById(id);
+        mentorrepository.deleteById(id);    	
+        return m;
+    }
 }
