9c3f42e9fbfdab8bda7070866b952abfd8aa372b
Sakibul Hasan <sakib.iut@gmail.com>
2019-07-08 20:30:45 -0400

    cleanup

--- src/main/java/co/getmehired/getmehired/rest/MentorResource.java
+++ src/main/java/co/getmehired/getmehired/rest/MentorResource.java
@@ -18,75 +18,61 @@ import co.getmehired.getmehired.service.MentorService;
 
 @RestController
 public class MentorResource {
-	
+
 	@Autowired
 	private MentorService mentorService;
-	
+
 	@PostMapping("/api/mentors")
 	public Mentor saveMentor(@Validated @RequestBody Mentor mentor) {
-	
-		 /// saving the input of mentor in the repository
 		mentor=mentorService.saveMentor(mentor);
 		return mentor;
 	}
-	
+
 	@GetMapping("/api/mentors")
 	public List<MentorDTO> getMentors(){
-		
-	    List<MentorDTO> mentor_dtos= new ArrayList<>();
-	    List<Mentor> mentors=  mentorService.getMentors();
-	    
-	    for (Mentor m:mentors) {
-	      
-	             MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
-	    			                  m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
-	    			                  m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
-	    			                  m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
-	             mentor_dtos.add(m_dto);
-	    }
-	    return mentor_dtos;
-	    
-    }
-	
+		List<MentorDTO> mentor_dtos= new ArrayList<>();
+		List<Mentor> mentors=  mentorService.getMentors();
+
+		for (Mentor m:mentors) {
+			MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+					m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+					m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+					m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+			mentor_dtos.add(m_dto);
+		}
+		return mentor_dtos;
+	}
+
 	@GetMapping("/api/mentors/{id}")
 	public MentorDTO getMentorById (@PathVariable String id) {
 		Mentor m=mentorService.getMentorsById(id).orElseGet(null);
-		//Mentor m=mentorService.getMentorsByName(id).orElseGet(null);
-	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
-                  m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
-                  m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
-                  m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
-	    return m_dto;
-		
-		
+		MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+				m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+				m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+				m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+		return m_dto;
 	}
+	
 	@PutMapping("/api/mentors/{id}")
 	public MentorDTO updateMentor(@PathVariable String id,@RequestBody Mentor m) {
-		
-		//Mentor old_mentor=mentorService.getMentorsById(id).orElseGet(null);
-		
-	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
-                 m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
-                 m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
-                 m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
-	    mentorService.saveMentor(m);
-	    mentorService.removeMentorById(id);
-	    return m_dto;	
+		MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+				m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+				m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+				m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+		mentorService.saveMentor(m);
+		mentorService.removeMentorById(id);
+		return m_dto;	
 	}
-	
+
 	@DeleteMapping("/api/mentors/{id}")
 	public MentorDTO deleteMentor(@PathVariable String id){
-      
 		Mentor m=mentorService.getMentorsById(id).orElseGet(null);
-	    MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
-                m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
-                m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
-                m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
-	    mentorService.removeMentorById(id);
-	    return m_dto;
-	    
-	    
+		MentorDTO m_dto=new MentorDTO(m.getId(),m.getNameMentor(),m.getPhoneNumberMentor(),m.getEmailAddressMentor(),
+				m.getAddressMentor(),m.getCalendlyUrlMentor(),m.getTimezoneMentor(),m.getSsnNumber(),m.getBankAccountMentor(),
+				m.getRoutingNumberMentor(),m.getAccademicDegreeMentor(),m.getDegreeSubjectMentor(),m.getMentorExpertise(),m.getName(),
+				m.getNoteTalent(),m.getTalentStatus(),m.getMeetingTopic());
+		mentorService.removeMentorById(id);
+		return m_dto;
 	}
-	
 
 }
--- src/main/java/co/getmehired/getmehired/rest/TalentResource.java
+++ src/main/java/co/getmehired/getmehired/rest/TalentResource.java
@@ -21,92 +21,67 @@ import co.getmehired.getmehired.service.TalentService;
 @CrossOrigin
 @RestController
 public class TalentResource {
-	
+
 	@Autowired
 	private TalentService talentService;
-	
+
 	@PostMapping("/api/talents")
 	public Talent saveTalent(@Validated @RequestBody Talent talent) {
-		
 		talent = talentService.save(talent);
 		return talent;
-	
 	}
 
-	
 	@GetMapping("/api/talents")
 	public List<TalentDTO> getTalents() {
-		
 		List<TalentDTO> talentDTOs = new ArrayList<>();
-		
+
 		List<Talent> talents = talentService.getTalents();
-		
+
 		for (Talent t : talents) {
-	    TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
-	    		t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
-	    		t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
-	    		t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
-			//dto.setId(t.getId());
-			//dto.setName(t.getName());
-			//dto.setEmailAddress(t.getEmailAddress());
-			//dto.setPhoneNumber(t.getPhoneNumber());
-			//TODO: HW: add all other fields (use a method)
-			
-		     talentDTOs.add(dto);
+			TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
+					t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
+					t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
+					t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
+
+			talentDTOs.add(dto);
 		}
 		return talentDTOs;
 	}
 
 	@GetMapping("/api/talents/{id}")
 	public TalentDTO getTalent(@PathVariable String id) {
-		
 		Talent t = talentService.getTalentById(id).orElseGet(null);
-//		Talent t = talentService.getTalentByName(id).orElseGet(null);
-		
-		 TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
-		    		t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
-		    		t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
-		    		t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
-		//dto.setId(t.getId());
-		///dto.setName(t.getName());
-		//dto.setEmailAddress(t.getEmailAddress());
-		//dto.setPhoneNumber(t.getPhoneNumber());
-		//TODO: HW: add all other fields (use a method)
-		//System.out.print("hommeeee  id");
+
+		TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
+				t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
+				t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
+				t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
+
 		return dto;
 	}
 
-
-//TODO: Homework	
 	@PutMapping("/api/talents/{id}")
 	public TalentDTO updateTalent(@PathVariable String id,@RequestBody Talent t) {
-		//  Talent old_talent = talentService.getTalentById(id).orElseGet(null);
-		  /// making a dto talent from new input talent
-		 TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
-		    		t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
-		    		t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
-		    		t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
-		  // saving the new talent in repo 
-		    talentService.save(t);
-		    talentService.removeTalentById(id);
-		    return dto;
-		   
-			
-		}
-	
-    @DeleteMapping("/api/talents/{id}")
+		TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
+				t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
+				t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
+				t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
+		
+		talentService.save(t);
+		talentService.removeTalentById(id);
+		return dto;
+	}
+
+	@DeleteMapping("/api/talents/{id}")
 	public TalentDTO deleteTalent(@PathVariable String id) {
-		   
-		   // search in the repo which talent matches the given id and make a talent
-		   Talent t = talentService.getTalentById(id).orElseGet(null);
-		 /// making a dto talent from that talent
-		   TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
-		    		t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
-		    		t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
-		    		t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
-		   
-		   /// delete the talent from repo
-		   talentService.removeTalentById(id);
-		   return dto;
-		}
+		Talent t = talentService.getTalentById(id).orElseGet(null);
+
+		TalentDTO dto=new TalentDTO(t.getId(),t.getName(),t.getPhoneNumber(),t.getEmailAddress(),t.getTimezone(),t.getImmigrationExpiaryStr(),
+				t.getImmigrationExpiary(),t.getAddress(),t.getSsnNumber(),t.getBankAccount(),t.getRoutingNumber(),t.getCitizenship(),t.getImmigrationStatus(),
+				t.getAccademicDegree(),t.getDegreeSubject(),t.getGraduationDate(),t.getGraduationDateStr(),t.getSuuportNeeded(),t.getObjective(),t.getEmploymentStat(),t.getSalaryStart(),t.getCurrentJob(),
+				t.getCurrentEmployer(),t.getJobSalary(),t.getNewEmployer(),t.getNewPosition(),t.getJobStartdate(),t.getJobStartdateStr());
+
+		talentService.removeTalentById(id);
+		return dto;
+	}
 }
