f7c77dce941a53b828c2385f94e9acb9b63b303a
asifahmed90 <asifahmed90@users.noreply.github.com>
2019-07-15 14:39:48 -0500

    changed pom to previous

--- src/main/java/co/getmehired/getmehired/GetMeHiredApplication.java
+++ src/main/java/co/getmehired/getmehired/GetMeHiredApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GetMeHiredApplication {
 
-	public static void main(String[] args) {SpringApplication.run(GetMeHiredApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(GetMeHiredApplication.class, args);
 	}
 
 }
-
