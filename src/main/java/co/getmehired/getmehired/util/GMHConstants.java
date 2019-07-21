bf675d7c47f8b63f14ef7b31e2644707b4cdaa8c
supal <supal.iut03@gmail.com>
2019-07-21 23:14:11 +0600

    removed key

--- src/main/java/co/getmehired/getmehired/util/GMHConstants.java
+++ src/main/java/co/getmehired/getmehired/util/GMHConstants.java
@@ -3,8 +3,8 @@ package co.getmehired.getmehired.util;
 public class GMHConstants {
 
     public static final String BUCKET_NAME = "getmehired-batch01";  // Your bucket name
-    public static final String S3_ACCESS_KEY = "AKIA3OZH3SI3V3ER4X7I"; // Your access key
-    public static final String S3_SECRET_KEY = "1xbSIWjfUqsmeT1bcHedEntR+VVcVlarqqTGsZCE";
+    public static final String S3_ACCESS_KEY = ""; // Your access key
+    public static final String S3_SECRET_KEY = "";
 
     public static final String STATUS_VALID = "V";
     public static final String STATUS_DELETED = "DELETED";
